55605d4f86f96de9d5a1deeb1eb37ec9e82f8252
Erik Vesterberg <erik.vesterberg@qred.com>
2020-04-04 17:41:31 +0200

    :bug: another try

--- src/loanCalculator.js
+++ src/loanCalculator.js
@@ -135,13 +135,8 @@ export default class LoanCalculator {
     if (firstMonthFree) {
       term -= 1
     }
-    if (market === 'nl' || market === 'fi') {
-      console.log(market, '1')
-       unformatted = amount + Math.min(amount, 10000) * (interest / 100) * term + Math.max((amount - 13), 0) * (scalingRate / 100) * term
-    } else {
-      console.log(market, '2')
-       unformatted = amount + Math.min(amount, 100000) * (interest / 100) * term + Math.max((amount - 100000), 0) * (scalingRate / 100) * term
-    }
+    console.log(market)
+    const unformatted = amount + Math.min(amount, 100000) * (interest / 100) * term + Math.max((amount - 100000), 0) * (scalingRate / 100) * term
 
     return {
       value: formatMoney(unformatted),
